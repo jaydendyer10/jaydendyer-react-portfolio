8b7c10923cc6587343f813689c9ba3aa8c66618f
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-10-15 16:29:19 -0600

    added array function

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -4,15 +4,25 @@ import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
-
+        
         console.log("Portfolio container has rendered")
     }
-    
+
+    portfolioItems() {
+        const data = ["Quip", "Eventbrite", "Ministry Safe"];
+
+        return data.map(item => {
+            return <PortfolioItem/>;
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio items go here..</h2>
-                <PortfolioItem/>
+                
+
+                {this.portfolioItems()}
             </div>
 
         );
