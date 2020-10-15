616a0dcc9f6999fa65092c8816d2020721bd0ba4
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-10-15 16:13:24 -0600

    added constructor

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        console.log("Portfolio container has rendered")
+    }
+    
     render() {
         return (
             <div>
