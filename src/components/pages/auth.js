83b19f687e4404dc069dd9b4a1cb4e37fccc987c
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-10-22 16:48:38 -0600

    import static images

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
 
 class Auth extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
   render() {
-    return Auth;
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`,
+          }}
+        />
+
+        <div className="right-column">
+          <h1>Login component goes here</h1>
+        </div>
+      </div>
+    );
   }
 }
 
