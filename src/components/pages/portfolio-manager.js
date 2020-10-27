c9ccf34db7022e2a2247997a3734581fba1dfa5a
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-10-27 17:12:01 -0600

    font awesome

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -16,11 +16,27 @@ export default class PortfolioManager extends Component {
       this
     );
     this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
-    this.handleDeleteClick = this.handleFormSubmissionError.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
   }
 
   handleDeleteClick(portfolioItem) {
-    console.log("handleDeleteClick", portfolioItem);
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.setState({
+          portfolioItems: this.state.portfolioItems.filter((item) => {
+            return item.id !== portfolioItem.id;
+          }),
+        });
+
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("handleDeleteClick error", error);
+      });
   }
 
   handleSuccessfulFormSubmission(portfolioItem) {
@@ -66,7 +82,7 @@ export default class PortfolioManager extends Component {
         </div>
         <div className="right-column">
           <PortfolioSidebarList
-            handleDeleteCLick={this.handleDeleteClick}
+            handleDeleteClick={this.handleDeleteClick}
             data={this.state.portfolioItems}
           />
         </div>
